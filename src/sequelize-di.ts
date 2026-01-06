1fbac5f839eda9cf7843c7a9b53c1c7879e4386a
lenconda <i@lenconda.top>
2026-01-06 19:01:49 +0800

    feat: 🎸 index

--- src/sequelize-di.ts
+++ src/sequelize-di.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 /* eslint-disable @typescript-eslint/no-unsafe-function-type */
-import { StringUtil } from '@open-norantec/utilities/dist/string-util.class';
-import { BelongsToOptions, ModelAttributeColumnOptions } from 'sequelize';
+import { BelongsToOptions, IndexesOptions, ModelAttributeColumnOptions } from 'sequelize';
 import {
   BelongsTo as SequelizeBelongsTo,
   Model,
@@ -15,45 +14,30 @@ import { Constructor } from 'type-fest';
 
 export * from 'sequelize-typescript';
 
-export interface TableOptions<M extends Model = Model> extends SequelizeTableOptions<M> {
-  standaloneIndexes?: Array<string | { field: string; name?: string; unique?: string }>;
+export interface TableOptions<M extends Model = Model> extends Omit<SequelizeTableOptions<M>, 'indexes'> {
+  indexes?: Array<IndexesOptions | string>;
 }
 
-export function Table<M extends Model = Model>({ standaloneIndexes, ...options }: TableOptions<M>) {
+export function Table<M extends Model = Model>({ indexes, ...options }: TableOptions<M>) {
   return (target: Constructor<M>) => {
-    const newOptions = !options ? {} : options;
-
-    if (!Array.isArray(newOptions?.indexes)) newOptions.indexes = [];
-
-    newOptions.indexes = Array.from(newOptions.indexes)
-      .concat({
-        name: 'pagination',
-        fields: ['id', 'created_at'],
-      })
-      .concat(
-        Array.isArray(standaloneIndexes)
-          ? standaloneIndexes
-              .map((item, index) => {
-                if (typeof item === 'string') {
-                  if (item.length === 0) return null;
-                  return {
-                    name: `idx_standalone__${index}`,
-                    fields: [item],
-                  };
-                } else if (typeof item?.field === 'string' && item.field.length > 0) {
-                  return {
-                    fields: [item.field],
-                    name: StringUtil.isFalsyString(item?.name) ? `idx_standalone__${index}` : item.name,
-                    unique: typeof item?.unique === 'boolean' ? item.unique : false,
-                  };
-                } else {
-                  return null;
-                }
-              })
-              .filter((item) => item !== null)
-          : [],
-      );
-
+    const newOptions: SequelizeTableOptions<M> = !options ? {} : options;
+    newOptions.indexes = [];
+    newOptions.indexes = (
+      Array.from(Array.isArray(indexes) ? indexes : []).map((item, index) => {
+        if (typeof item === 'string') {
+          return [
+            {
+              name: `sidx__${index}`,
+              fields: [item],
+            },
+          ];
+        }
+        return item;
+      }) as IndexesOptions[]
+    ).concat({
+      name: 'pagination',
+      fields: ['id', 'created_at'],
+    });
     SequelizeTable({
       ...newOptions,
       tableName: newOptions?.modelName,
